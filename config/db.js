55b46590b809f1517c91d3cb67871ab3c7cc71b3
ruthvik <131393980+ruthvik15@users.noreply.github.com>
2025-04-18 12:18:01 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 
-const connectDB = async () => {
+const connectDB = async (MONGO_URI) => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/ragapp2");
+    await mongoose.connect(MONGO_URI);
     console.log('✅ MongoDB Connected');
   } catch (err) {
     console.error('❌ MongoDB Connection Error:', err.message);
